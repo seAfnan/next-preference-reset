953159a1796807356388bd3951ec317681bacca8
Afnan A <hafnan03@gmail.com>
2024-04-15 07:47:37 +0500

    Title, Preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Theme } from "@radix-ui/themes";
 import { Inter } from "next/font/google";
 import DarkModeContext from "./DarkModeContext";
 import NavBar from "./NavBar";
+import meta from "@/public/meta.gif";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,6 +26,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>Reset your preferences | Next.js</title>
+      <meta name="title" content="Reset your preferences | Next.js" />
+      <meta
+        name="description"
+        content="Simple app to show how you can save your deeds."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://next-preference-reset.vercel.app/"
+      />
+      <meta property="og:title" content="Reset your preferences | Next.js" />
+      <meta
+        property="og:description"
+        content="Simple app to show how you can save your deeds."
+      />
+      <meta property="og:image" content={meta.src} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://next-preference-reset.vercel.app/"
+      />
+      <meta
+        property="twitter:title"
+        content="Reset your preferences | Next.js"
+      />
+      <meta
+        property="twitter:description"
+        content="Simple app to show how you can save your deeds."
+      />
+      <meta property="twitter:image" content={meta.src} />
+
       <body className={inter.variable}>
         <DarkModeContext>
           <Theme accentColor="iris" radius="none">
